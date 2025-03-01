12a6b78badcbb23cf49ea82fafc404e84c671af5
zaidayad <zaidayad11@gmail.com>
2025-03-02 01:55:19 +0300

    إصلاح خطأ DisallowedHost

--- shop/urls.py
+++ shop/urls.py
@@ -7,7 +7,7 @@ from .views import (cart_view, login_view, product_list, register,
 
 urlpatterns = [
     path("register/", register, name="register"),
-    path("login/", login_view, name="login"),
+    path("", login_view, name="login"),
     path("categories/", category_list, name="category_list"),
     path("categories/<int:category_id>/", product_list, name="product_list"),
     path("add_to_cart/<int:product_id>/", add_to_cart, name="add_to_cart"),
